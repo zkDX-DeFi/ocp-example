15083952f926ccf5a0534ab68e3ca3845d383a4a
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-14 13:46:36 +0800

    Update MockPair2.sol

--- contracts/mock/MockUniswap/MockPair2.sol
+++ contracts/mock/MockUniswap/MockPair2.sol
@@ -88,4 +88,30 @@ contract MockPair2 is ERC20{
         blockTimestampLast = blockTimestamp;
         emit Sync(reserve0, reserve1);
     }
+
+    function mint(address _to) external lock returns(uint _liquidity) {
+        (uint112 _reserve0, uint112 _reserve1, ) = getReserves();
+
+        uint balance0 = IERC20(token0).balanceOf(address(this));
+        uint balance1 = IERC20(token1).balanceOf(address(this));
+        uint amount0 = balance0 - _reserve0;
+        uint amount1 = balance1 - _reserve1;
+
+        bool feeOn = _mintFee(_reserve0, _reserve1);
+        uint _totalSupply = totalSupply();
+
+        if (_totalSupply == 0) {
+            _liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
+            _mint(address(0), MINIMUM_LIQUIDITY);
+        } else {
+            _liquidity = Math.min(amount0 * _totalSupply / _reserve0, amount1 * _totalSupply / _reserve1);
+        }
+        require(_liquidity > 0, "INSUFFICIENT_LIQUIDITY_MINTED");
+        _mint(_to, _liquidity);
+
+        _update(balance0, balance1, _reserve0, _reserve1);
+        if (feeOn) kLast = uint(reserve0) * uint(reserve1);
+
+        emit Mint(msg.sender, amount0, amount1);
+    }
 }
