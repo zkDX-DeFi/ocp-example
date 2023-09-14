483ab23bd84c9c430f6b73c92c82ddedafa33ff5
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-14 14:57:26 +0800

    Update MockPair.sol

--- contracts/mock/MockUniswap/MockPair.sol
+++ contracts/mock/MockUniswap/MockPair.sol
@@ -1,13 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
+import "./UQ112x112.sol";
 
 contract MockPair is ERC20{
+    using UQ112x112 for uint224;
+
+    uint public constant MINIMUM_LIQUIDITY = 10**3;
+    bytes4 private constant SELECTOR = bytes4(keccak256(bytes('transfer(address,uint256)')));
 
     address public factory;
     address public token0;
     address public token1;
+
+    uint112 private reserve0;
+    uint112 private reserve1;
+    uint32 private blockTimestampLast;
+
+    uint public price0CumulativeLast;
+    uint public price1CumulativeLast;
+    uint public kLast;
+    uint private unlocked = 1;
+
+    modifier lock() {
+        require(unlocked == 1, "LOCKED");
+        unlocked = 0;
+        _;
+        unlocked = 1;
+    }
 
     constructor () ERC20("MockPair", "MP"){
         factory = msg.sender;
