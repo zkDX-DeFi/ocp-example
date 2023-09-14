3d3d62b1f507cb475c10f1a6c657aa4b5bcae22d
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-14 14:58:53 +0800

    Update MockPair.sol

--- contracts/mock/MockUniswap/MockPair.sol
+++ contracts/mock/MockUniswap/MockPair.sol
@@ -39,4 +39,13 @@ contract MockPair is ERC20{
         token0 = _token0;
         token1 = _token1;
     }
+
+    function getReserves() public view returns(
+        uint112 _reserve0,
+        uint112 _reserve1,
+        uint32 _blockTimestampLast) {
+        _reserve0 = reserve0;
+        _reserve1 = reserve1;
+        _blockTimestampLast = blockTimestampLast;
+    }
 }
