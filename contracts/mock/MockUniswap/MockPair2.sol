43d3588038920ae3a9e49dab0ff18331a92bed61
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-14 13:39:44 +0800

    Update MockPair2.sol

--- contracts/mock/MockUniswap/MockPair2.sol
+++ contracts/mock/MockUniswap/MockPair2.sol
@@ -72,5 +72,20 @@ contract MockPair2 is ERC20{
         }
     }
 
-    
+    function _update(uint balance0, uint balance1, uint112 _reserve0, uint112 _reserve1) private {
+        require(balance0 <= type(uint112).max && balance1 <= type(uint112).max, "OVERFLOW");
+        uint32 blockTimestamp = uint32(block.timestamp % 2**32);
+
+        uint32 timeElapsed = blockTimestamp - blockTimestampLast;
+        if (timeElapsed > 0 && _reserve0 != 0 && _reserve1 != 0) {
+            price0CumulativeLast += uint(UQ112x112.encode(_reserve1).uqdiv(_reserve0)) * timeElapsed;
+            price1CumulativeLast += uint(UQ112x112.encode(_reserve0).uqdiv(_reserve1)) * timeElapsed;
+        }
+
+        reserve0 = uint112(balance0);
+        reserve1 = uint112(balance1);
+
+        blockTimestampLast = blockTimestamp;
+        emit Sync(reserve0, reserve1);
+    }
 }
