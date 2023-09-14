53a73c53317c385685aea022f365ff9451fe175c
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-14 13:58:08 +0800

    submitted

--- contracts/mock/MockUniswap/MockFactory.sol
+++ contracts/mock/MockUniswap/MockFactory.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "./MockPair2.sol";
+
 contract MockFactory {
     mapping(address => mapping(address => address)) public getPair; // mapping from tokenA to tokenB to pair address
     address[] public allPairs; // array with all pairs
@@ -23,7 +25,26 @@ contract MockFactory {
         return allPairs.length;
     }
 
-    function createPair(address _tokenA, address _tokenB) external returns(address _pair) {
+    function createPair(address _token0, address _token1) external returns(address _pair) {
+        require(_token0 != _token1, "MockFactory: IDENTICAL_ADDRESSES");
+        (address token0, address token1) = _token0 < _token1 ? (_token0, _token1) : (_token1, _token0);
 
+        require(token0 != address(0), "MockFactory: ZERO_ADDRESS");
+        require(getPair[token0][token1] == address(0), "MockFactory: PAIR_EXISTS");
+
+        bytes memory bytecode = type(MockPair2).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+
+        assembly {
+            _pair := create2(0, add(bytecode,32), mload(bytecode), salt)
+        }
+
+        MockPair2(_pair).initialize(token0, token1);
+
+        getPair[token0][token1] = _pair;
+        getPair[token1][token0] = _pair;
+
+        allPairs.push(_pair);
+        emit PairCreated(token0, token1, _pair, allPairs.length);
     }
 }
